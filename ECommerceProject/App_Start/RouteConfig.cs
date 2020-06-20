0edc64be01c76203aa612781eb480b5a2220fed7
Vacip <vacipderici@gmail.com>
2020-06-20 17:50:02 +0300

    Layout Giydirildi.

--- ECommerceProject/App_Start/RouteConfig.cs
+++ ECommerceProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ECommerceProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "i", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
