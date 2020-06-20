98e182320325f36abc12e2752d833ff54d3bab65
Vacip <vacipderici@gmail.com>
2020-06-20 18:02:32 +0300

    DD  bağlantısı sağlandı.

--- ECommerceProject/Controllers/iController.cs
+++ ECommerceProject/Controllers/iController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceProject.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,16 @@ namespace ECommerceProject.Controllers
     public class iController : Controller
     {
         // GET: i
+        ETicaretEntities context;
+
+        public iController()
+        {
+            context = new ETicaretEntities();
+        }
+
         public ActionResult Index()
         {
+            
             return View();
         }
     }
---
+++ ECommerceProject/Models/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceProject.Models
+{
+    public enum Enums:int
+    {
+        Customer = 0,
+        Admin=10,
+        Editor=8
+    }
+}
